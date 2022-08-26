49c750690c7f70f2f9cb2cd5f392beec1456c41d
Luiz Carlos <luidestec@outlook.com>
2022-08-25 23:36:43 -0300

    EndPoints Get

--- Controllers/PedidoExameController.cs
+++ Controllers/PedidoExameController.cs
@@ -1,19 +1,17 @@
 ﻿using LabClinic.Entitie;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace LabClinic.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class PedidoExameController : ControllerBase
-    {
-        [HttpGet]
-        [Route("PedidoDeExame")]
-        public async Task<IActionResult> GetExame() {
 
-            var exame = new List<Exame>()
-            {
+    {
+        private List<Exame> exames = new List<Exame>()
+        {
                 new Exame()
                 {
                     Codigo = 1,
@@ -21,18 +19,35 @@ namespace LabClinic.Controllers
                     Valor = 30,
 
                 },
-                 new Exame()
+                new Exame()
                 {
                     Codigo = 2,
                     Descricao = "Cardiograma",
                     Valor = 60,
-
                 }
 
-            };
+        };
+
+        [HttpGet]
+        [Route("PedidoDeExames")]
+        public async Task<IActionResult> GetExame()
+        {
+
+            return Ok(exames);
+        }
+
+        [HttpGet]
+        [Route("PedidoDeExame")]
+        public async Task<IActionResult> GetExame(int codigo)
+        {
+            var exame = exames.Find(x => x.Codigo == codigo);
+            if(exame == null)
+            {
+                return BadRequest("Exame não encontrado!");
+            }
             return Ok(exame);
         }
 
-       
+
     }
 }
